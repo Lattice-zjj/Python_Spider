338e655cf87083a7670ee3815d3be79f410e3bf9
zhou <2186283744@qq.com>
2020-10-06 11:00:57 +0800

    second commit

--- 笔趣阁小说爬取.py
+++ 笔趣阁小说爬取.py
@@ -9,16 +9,19 @@ if __name__ == "__main__":
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
     }
 
-    url = 'http://www.biquge.info/32_32534/'
+    url = 'http://www.biquge.info/1_1245/'
     response = requests.get(url=url, headers=headers)
     response.encoding = 'utf-8'
     page_text = response.text
-
+    tree_page = etree.HTML(page_text)
+    book_name = tree_page.xpath(
+        '/html/body/div/div[5]/div[2]/div[1]/h1/text()')
     # 实例化
     soup = BeautifulSoup(page_text, 'lxml')
     dd_list = soup.select('.box_con > div > dl > dd')
-    fp = open('./左道倾天.txt', 'w', encoding='utf-8')
-    for dd in dd_list[400:]:
+    FileName = './'+book_name[0]+'.txt'
+    fp = open(FileName, 'w', encoding='utf-8')
+    for dd in dd_list:
         title = dd.a.string
         fp.write(title+'\n\n')
         detail = url+dd.a['href']
